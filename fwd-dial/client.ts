9b88e75a2cbb808754d1f04423abbb337078255a
ross-inksmith <rweir@inksmith.ca>
2023-04-26 12:25:27 -0400

    Extend class rather than wrap

--- fwd-dial/client.ts
+++ fwd-dial/client.ts
@@ -2,29 +2,43 @@ namespace modules {
   /**
    * Wrapper for FWD Edu rotary encoder, "Dial"
    **/
-  export class FwdEduDial {
-    private _dialClient: modules.RotaryEncoderClient
+  //% fixedInstances
+  export class FwdEduDial extends modules.RotaryEncoderClient {
     private _cwAction: (delta: number) => void
     private _ccwAction: (delta: number) => void
 
     constructor(role: string) {
-      this._dialClient = new modules.RotaryEncoderClient(role)
+      super(role)
+
+      this._cwAction = (_) => {}
+      this._ccwAction = (_) => {}
+
+      this.onReadingChangedBy(1, (delta) => {
+        if (delta < 0) {
+          this._cwAction(delta)
+        } else {
+          this._ccwAction(delta)
+        }
+      })
+
     }
     
     //% group="Dial"
-    //% block="clicks per full turn"
+    //% block="%dial clicks per full turn"
     //% blockId=fwd_dial_get_clicks_per_turn
     //% color="#00cc00"
-    clicksPerTurn(): number { return this._dialClient.clicksPerTurn() }
+    clicksPerTurn(): number { return this.clicksPerTurn() }
     
     //% group="Dial"
-    //% block="%dial position"
+    //% block="%rotaryencoder position"
     //% blockId=fwd_dial_get_position
     //% color="#00cc00"
-     position(): number { return this._dialClient.position() }
+     position(): number { return this.position() }
     
     
 
   }
 
+  //% fixedInstance whenUsed weight=1 block="dial1"
+  export const dial1 = new FwdEduDial("dial1")
 }
