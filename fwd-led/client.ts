14abda4f7162a3f1a514049e9c6ab5e8cb2b77b8
ross-inksmith <rweir@inksmith.ca>
2023-04-27 13:28:06 -0400

    Block/Service brightness mapping, block changes

--- fwd-led/client.ts
+++ fwd-led/client.ts
@@ -1,25 +1,32 @@
 //% block="Light Ring" color=#cc00cc
 namespace fwdEduLED {
-  const MAX_BRIGHTNESS = 10
+  const MAX_REPORT_BRIGHTNESS = 10
+  const MAX_SERVICE_BRIGHTNESS = 100
+  const toBlocksBrightness = ( serviceBrightness: number ): number => { return MAX_REPORT_BRIGHTNESS * serviceBrightness / MAX_SERVICE_BRIGHTNESS } 
+  const toServiceBrightness = ( reportBrightness: number ): number => { return MAX_SERVICE_BRIGHTNESS * reportBrightness / MAX_REPORT_BRIGHTNESS } 
 
   //% fixedInstances
   export class FwdEduLEDClient extends modules.LedClient {
+    
 
     constructor(role: string) {
       super(role)
     }
 
-    //% block
+    //% block="set brightness to $value"
+    //% $value.min=0
+    //% $value.max=10
+    //% $value.defl=10
     fwdSetBrightness(value: number): void {
-      this.setBrightness(value / MAX_BRIGHTNESS)
+      this.setBrightness(toServiceBrightness(value))
     }
 
-    //% block="$ledRing brightness"
+    //% block="brightness"
     fwdBrightness(): number {
-      return this.brightness() * MAX_BRIGHTNESS
+      return toBlocksBrightness(this.brightness())
     }
 
-    //% block
+    //% block="
     fwdRotate(offset = 1): void { this.rotate(offset) }
 
     //% block
