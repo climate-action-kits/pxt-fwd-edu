33643b1fe968b9292e1e37b4d9252581be1f6671
ross-inksmith <rweir@inksmith.ca>
2023-04-27 15:13:30 -0400

    Add LED functions
    TODO: blocks are broken

--- fwd-led/client.ts
+++ fwd-led/client.ts
@@ -5,20 +5,41 @@ namespace fwdEduLED {
   const toBlocksBrightness = ( serviceBrightness: number ): number => { return MAX_REPORT_BRIGHTNESS * serviceBrightness / MAX_SERVICE_BRIGHTNESS } 
   const toServiceBrightness = ( reportBrightness: number ): number => { return MAX_SERVICE_BRIGHTNESS * reportBrightness / MAX_REPORT_BRIGHTNESS } 
 
+  export const enum pixelNames {
+		//% block="PXL1"
+    PXL1 = 0,
+		//% block="PXL2"
+    PXL2 = 1,
+		//% block="PXL3"
+    PXL3 = 2,
+		//% block="PXL4"
+    PXL4 = 3,
+		//% block="PXL5"
+    PXL5 = 4,
+		//% block="PXL6"
+    PXL6 = 5,
+		//% block="PXL7"
+    PXL7 = 6,
+		//% block="PXL8"
+    PXL8 = 7,
+  }
+
   //% fixedInstances
   export class FwdEduLEDClient extends modules.LedClient {
-    
 
     constructor(role: string) {
       super(role)
     }
 
     //% block="set brightness to %value"
-    //% value.min=0
-    //% value.max=10
-    //% value.defl=10
+    //% value.min=0 value.max=10 value.defl=10
     fwdSetBrightness(value: number): void {
       this.setBrightness(toServiceBrightness(value))
+    }
+
+    //% block="number of pixels"
+    fwdNumPixels(): number {
+      return this.numPixels();
     }
 
     //% block="brightness"
@@ -26,7 +47,13 @@ namespace fwdEduLED {
       return toBlocksBrightness(this.brightness())
     }
 
-    //% block="
+    //% block="set %index to %rgb=colorPicker
+    fwdSetPixelColour(index: pixelNames | number, rgb: number): void { this.setPixelColor(index, rgb) }
+
+    //% block="set all LEDs to %rgb=colorPicker
+    fwdSetAllPixelsColour(rgb: number): void { this.setAll(rgb) }
+
+    //% block
     fwdRotate(offset = 1): void { this.rotate(offset) }
 
     //% block
