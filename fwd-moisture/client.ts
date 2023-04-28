c289fc2d318ad327ede00235e8a7c95cf383cc6e
ross-inksmith <rweir@inksmith.ca>
2023-04-28 10:12:47 -0400

    Initial soil moisture

---
+++ fwd-moisture/client.ts
@@ -0,0 +1,43 @@
+namespace fwdSensors {
+
+  //% fixedInstances
+  export class FwdSoilMoistureClient extends modules.SoilMoistureClient {
+
+    constructor(role: string) {
+      super(role)
+    }
+
+    /**
+    */
+    //% group="Moisture"
+    //% block="%soilmoisture moisture level (\\%)"
+    fwdMoistureLevel(): number { this.moisture() }
+
+    /**
+    */
+    //% group="Moisture"
+    //% block="on %soilmoisture moisture level changed by %threshold (\\%)"
+    //% weight=97
+    //% threshold.min=0 threshold.max=100 threshold.defl=5
+    onMoistureLevelChangedBy(threshold: number, handler: () => void): void {
+      this.onReadingChangedBy(threshold, handler)
+    }
+
+    /**
+     */
+    //% group="Moisture"
+    //% block="on %soilmoisture moisture level %direction %threshold (\\%)"
+    //% threshold.min=0 threshold.max=100 threshold.defl=5
+    onLightLevelPastThreshold(threshold: number, direction: 'over' | 'under', handler: () => void): void {
+      this.onReadingChangedBy(0.01, (_) => {
+        const difference = this.moisture() - threshold > 0;
+        const isPastThreshold = 
+          direction === 'over' && difference ||
+          direction === 'under' && !difference;
+        return isPastThreshold && handler();
+      })
+    }
+
+  }
+
+}
