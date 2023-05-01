f901cbb988f4fa91b27ef9cf139b511f92c9baa7
ross-inksmith <rweir@inksmith.ca>
2023-05-01 13:12:54 -0400

    Dial testing

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
 //% block="Sensors" color=#80CE4E
-namespace fwdSensors {}
+//% weight=201
+namespace fwdSensors {
+  //export const enum thresholdDirection {
+  //  //% block="over"
+  //  over,
+  //  //% block="under"
+  //  under 
+  //}
+
+}
 
 //% block="Motors" color=#59BAE3
+//% weight=200
 namespace fwdMotors {}
