13f5d287b3f4e7a18ad84ed4117af2d47323b7a1
ross-inksmith <rweir@inksmith.ca>
2023-05-02 11:38:48 -0400

    LED: continued

--- fwd-led/client.ts
+++ fwd-led/client.ts
@@ -23,12 +23,12 @@ namespace fwdSensors {
 
   //% fixedInstances
   export class FwdLEDClient extends modules.LedClient {
-    static MAX_REPORT_BRIGHTNESS = 10
-    static MAX_SERVICE_BRIGHTNESS = 100
-    static toBlocksBrightness ( serviceBrightness: number ): number {
+    MAX_REPORT_BRIGHTNESS = 10
+    MAX_SERVICE_BRIGHTNESS = 100
+    toBlocksBrightness ( serviceBrightness: number ): number {
       return this.MAX_REPORT_BRIGHTNESS * serviceBrightness / this.MAX_SERVICE_BRIGHTNESS
     } 
-    static toServiceBrightness ( reportBrightness: number ): number {
+    toServiceBrightness ( reportBrightness: number ): number {
       return this.MAX_SERVICE_BRIGHTNESS * reportBrightness / this.MAX_REPORT_BRIGHTNESS
     } 
 
