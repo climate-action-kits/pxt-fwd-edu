805882f21ee6283b5be3d9ea12ffe281841fe67f
ross-inksmith <rweir@inksmith.ca>
2023-05-03 09:28:14 -0400

    Breakout: Init servo and relay

--- fwd-breakout/client.ts
+++ fwd-breakout/client.ts
@@ -8,11 +8,42 @@ namespace fwdMotors {
     constructor(role: string) {
       super(role)
     }
+
+    /**
+     */
+    //% group="Pump"
+    //% block="$this active"
+    //% blockId=fwd_relay_get_status
+    fwdIsActive(): boolean { return this.active() }
+
+    /**
+     */
+    //% group="Pump"
+    //% block="set $this $state"
+    //% blockId=fwd_relay_get_status
+    //% state.shadow="toggleOnOff"
+    fwdSetActive(state: boolean): void { this.setActive(state) }
+    
+    /**
+     */
+    //% group="Pump"
+    //% block="run $this for $duration"
+    //% blockId=fwd_relay_get_status
+    //% duration.shadow="timePicker"
+    fwdTimedRun(duration: number): void { control.inBackground(() => {
+        this.setActive(true)
+        basic.pause(duration)
+        this.setActive(false)
+      })
+    }
     
   }
 
+  //% fixedInstance
+  export const pump = new FwdRelayClient("pump")
+
   /**
-   **/
+   */
   //% fixedInstances
   export class FwdServoClient extends modules.ServoClient {
 
@@ -20,6 +51,42 @@ namespace fwdMotors {
       super(role)
     }
     
+    /**
+     */
+    //% group="Servo"
+    //% block="$this angle (°)"
+    //% blockId=fwd_servo_get_angle
+    fwdGetAngle(): void { return this.actualAngle() }
+
+    /**
+     */
+    //% group="Servo"
+    //% block="set $this to $angle °"
+    //% blockId=fwd_servo_set_angle
+    //% angle.min=-90 angle.max=90
+    fwdSetAngle(angle: number): void { this.setAngle(angle) }
+
+    /**
+     */
+    //% group="Servo"
+    //% block="$this state"
+    //% blockId=fwd_servo_is_enabled
+    fwdIsEnabled(): boolean { return this.enabled() }
+
+    /**
+     */
+    //% group="Servo"
+    //% block="set $this $state"
+    //% blockId=fwd_servo_is_enabled
+    //% state.shadow="toggleOnOff"
+    fwdSetEnabled(state: boolean): void { return this.setEnabled(state) }
+
   }
 
+  //% fixedInstance
+  export const servo1 = new FwdServoClient("servo 1")
+  //% fixedInstance
+  export const servo2 = new FwdServoClient("servo 2")
+  //% fixedInstance
+  export const servo3 = new FwdServoClient("servo 3")
 }
