526a862bef298fb99797450f51c047a18593199e
ross-inksmith <rweir@inksmith.ca>
2023-05-19 11:47:54 -0400

    Change drawer icons to match board

--- fwd-common/motors.ts
+++ fwd-common/motors.ts
@@ -1,3 +1,4 @@
 //% block="Motors" color=#59BAE3
+//% icon="\uf013"
 //% weight=200
 namespace fwdMotors {}
--- fwd-common/sensors.ts
+++ fwd-common/sensors.ts
@@ -1,4 +1,5 @@
 //% block="Sensors" color=#80CE4E
+//% icon="\uf8dd"
 //% weight=201
 namespace fwdSensors {
   export const enum thresholdDirection {
