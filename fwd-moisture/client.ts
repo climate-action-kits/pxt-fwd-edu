d0dd22e6b1bc9614c901421857d2a5c040029dc9
ross-inksmith <rweir@inksmith.ca>
2023-06-09 15:54:30 -0400

    Add: moisture level tooltips

--- fwd-moisture/client.ts
+++ fwd-moisture/client.ts
@@ -8,6 +8,7 @@ namespace fwdSensors {
     }
 
     /**
+     * Returns the sensor's moisture level as a number between 0-100
     */
     //% group="Moisture"
     //% block="$this level (\\%)"
@@ -15,14 +16,19 @@ namespace fwdSensors {
     fwdMoistureLevel(): number { return this.moisture() }
 
     /**
+     * Runs code when the moisture level changes by more than a certain amount between readings
+     * @param threshold how many percent two readings have to differ by before code is run
     */
     //% group="Moisture"
     //% block="on $this level changed by $threshold (\\%)"
     //% blockId=fwd_moisture_on_level_change
-    //% threshold.min=0 threshold.max=100 threshold.defl=5
+    //% threshold.min=1 threshold.max=100 threshold.defl=5
     fwdOnMoistureLevelChangedBy(threshold: number, handler: () => void): void { this.onReadingChangedBy(threshold, handler) }
 
     /**
+     * Runs code when the moisture level goes above or below a set threshold
+     * @param threshold what moisture level is the cut off before the code is run
+     * @param direction run when the level is above or below your set threshold
     */
     //% group="Moisture"
     //% block="is $this level $direction $threshold (\\%)"
