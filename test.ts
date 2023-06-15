edea34c0b65f1fb0b7da645193ddaf02f5cf790a
ross-inksmith <rweir@inksmith.ca>
2023-06-15 10:48:04 -0400

    Add tests for blocks unique to pxt-fwd-edu

--- test.ts
+++ test.ts
@@ -1,1 +1,48 @@
-//TODO: write tests
+function demoRunPumpDuration () {
+  console.info("Make sure the pump is connected to the board, and the board is on")
+  console.info("A heart will display on the microbit, and then random icons will cycle for 5s\n \
+               while the pump runs for 2 seconds in the background. This will repeat twice.")
+  let startTime = 0
+  while (!fwdMotors.pump.isConnected()) {
+      basic.pause(100)
+  }
+  for (let index = 0; index < 3; index++) {
+      basic.showIcon(IconNames.Heart)
+      startTime = control.millis()
+      fwdMotors.pump.fwdTimedRun(2000)
+      while (control.millis() - startTime < 5000) {
+          basic.showIcon(randint(3,20))
+      }
+      basic.clearScreen()
+      basic.pause(1000)
+  }
+}
+
+function demoSetAngleAndWait () {
+  console.info("Make sure a positional servo is connected to the left board connector, and the board is on")
+  console.info("A heart will display on the microbit, and then the servo arm will sweep between its maxima\n \
+               for 10 seconds. It should be able to reach its maxima before additional code gets run. This will repeat twice.")
+  let startTime = 0
+  while (!fwdMotors.servo1.isConnected()) {
+      basic.pause(100)
+  }
+  fwdMotors.servo1.setEnabled(true)
+  const minAngle = 0
+  const maxAngle = 270
+  console.log(`Move between ${minAngle} and ${maxAngle}`)
+  for (let index = 0; index < 3; index++) {
+      basic.showIcon(IconNames.Heart)
+      startTime = control.millis()
+      while (control.millis() - startTime < 10000) {
+          fwdMotors.servo1.fwdSetAngleAndWait(minAngle)
+          fwdMotors.servo1.fwdSetAngleAndWait(maxAngle)
+      }
+      basic.clearScreen()
+      basic.pause(1000)
+  }
+}
+
+console.info("Test new pump block")
+demoRunPumpDuration()
+console.info("Test new servo block")
+demoSetAngleAndWait()
